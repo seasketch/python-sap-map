cad256e2a110379c48ba0a68814cec9b57a6a591
Tim Welch <tim.j.welch@gmail.com>
2021-08-31 17:31:59 -0700

    split out calcSap, switch to geojson test input for accuracy, beef up simple test checks

--- lib/sapmap/gen_sap_map.py
+++ lib/sapmap/gen_sap_map.py
@@ -10,40 +10,8 @@ import simplejson
 import time
 import datetime
 from sapmap.calc_raster_props import calcRasterProps
+from sapmap.calc_sap import calcSap
 from rasterio.crs import CRS
-
-def calcSap(geometry, importance=1, areaFactor=1, importanceFactor=1, maxArea=None, maxSap=None):
-  """Calculates the SAP value given geometry, its importance, the area per cell, and an optional importanceFactor
-
-  Each respondent has a total SAP of 100, and each shape is assigned a portion
-  of that 100 The SAP for a shape is calculated as (Importance / Area), which
-  can be interpreted as "importance per area unit".  The area of the shape is
-  calculated in the coordinate system used, for Web Mercator that yields
-  square meters.  Use the areaFactor option to change this.
-  
-  Use the optional importanceFactor to accommodate additional variables, changing what the SAP represents.
-
-  Args:
-    geometry: Shapely geometry in 3857 (unit of meters)
-    importance: importance of geometry (1-100)
-    areaFactor: factor to change the area by dividing. For example if area of geometry is calculated in square meters, an areaFactor of 1,000,000 will make the SAP per square km. because 1 sq. km = 1000m x 1000m = 1mil sq. meters
-    importanceFactor: numeric value to multiply the importance by.  Use to scale the SAP from being 'per respondent' to a larger group of livelihoods or even economic values 
-    maxArea: limits the area of a shape.  Gives shapes with high area an artifically lower one, increasing their SAP relative to others, increasing their presence in heatmap
-    maxSap: limits the priority of shapes. Gives shapes with high priority an artificially lower one, decreasing their presence in heatmap
-  """
-  
-  area = geometry.area / areaFactor
-  
-  if (maxArea):
-    area = min(area, maxArea)
-
-  sap = importanceFactor * importance / area
-
-  if (maxSap):
-    sap = min(sap, maxSap)
-  
-  return sap
-
 
 def genSapMap(
   infile,
--- tests/test_simple_sap_map.py
+++ tests/test_simple_sap_map.py
@@ -6,15 +6,37 @@ def test_simple_sap_map():
     INPUT = os.path.join(os.path.dirname(os.path.abspath(__file__)), "input")
     OUTPUT = os.path.join(os.path.dirname(os.path.abspath(__file__)), "output")
 
-    shapes = os.path.join(INPUT, 'simple-polygon.shp')
+    shapes = os.path.join(INPUT, 'simple-polygon.geojson')
     raster = os.path.join(OUTPUT, 'simple-polygon.tif')
 
     assert(os.path.isfile(shapes))
-    genSapMap(shapes, raster, cellSize=100)
+    manifest = genSapMap(shapes, raster, cellSize=100)
     assert(os.path.isfile(raster))
+    assert(len(manifest['included']) == 5)
+
     with rasterio.open(os.path.join(OUTPUT, 'simple-polygon.tif')) as reader:
-        arr = reader.read()
+        assert(manifest['outBounds'][0] == reader.bounds.left)
+        assert(manifest['outBounds'][1] == reader.bounds.bottom)
+        assert(manifest['outBounds'][2] == reader.bounds.right)
+        assert(manifest['outBounds'][3] == reader.bounds.top)
         
+        assert(manifest['height'] == reader.height == 4)
+        assert(manifest['width'] == reader.width == 4)
+        assert(manifest['params']['cellSize'] == reader.res[0] == reader.res[1])
+        assert(reader.nodata == 0.0)
+
+        arr = reader.read()
+        # Hand verified values, view simple-polygon.tif in qgis using the tests/base/testdata.qgz project to verify
+        checkArr = [
+            [1,   0.5, 0,    0],
+            [0.5, 0,   0.5,  0],
+            [0,   0.5, 1.25, 0.25],
+            [0,   0,   0.25, 0.25]
+        ]
+        for rowId, row in enumerate(checkArr):
+            for colId, cell in enumerate(row):
+                # TODO: see if this can be extended out to 6 decimal places
+                assert(cell == checkArr[rowId][colId]) # Should be equal within 2 decimal places        
 
 if __name__ == "__main__":
     test_simple_sap_map()
