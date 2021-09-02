908718370019fcf4aadccc5d9475ec98f54c7427
Tim Welch <tim.j.welch@gmail.com>
2021-09-02 03:00:40 -0700

    test importance

--- tests/test_simple_sap_map.py
+++ tests/test_simple_sap_map.py
@@ -35,8 +35,43 @@ def test_simple_sap_map():
             [0,   0,   0.25, 0.25]
         ]], dtype=np.float32)
         np.testing.assert_array_equal(arr, checkArr)
+
+def test_importance_sap_map():
+    """Includes importanceField
+    """
+    INPUT = os.path.join(os.path.dirname(os.path.abspath(__file__)), "input")
+    OUTPUT = os.path.join(os.path.dirname(os.path.abspath(__file__)), "output")
+
+    shapes = os.path.join(INPUT, 'simple-polygon.geojson')
+    raster = os.path.join(OUTPUT, 'simple-polygon.tif')
+
+    assert(os.path.isfile(shapes))
+    manifest = genSapMap(shapes, raster, outResolution=100, importanceField='importance')
+    assert(os.path.isfile(raster))
+    assert(len(manifest['included']) == 5)
+
+    with rasterio.open(raster) as reader:
+        assert(manifest['outBounds'][0] == reader.bounds.left)
+        assert(manifest['outBounds'][1] == reader.bounds.bottom)
+        assert(manifest['outBounds'][2] == reader.bounds.right)
+        assert(manifest['outBounds'][3] == reader.bounds.top)
         
+        assert(manifest['height'] == reader.height == 4)
+        assert(manifest['width'] == reader.width == 4)
+        assert(manifest['params']['outResolution'] == reader.res[0] == reader.res[1])
+        assert(reader.nodata == 0.0)
+
+        arr = reader.read()
+        # Hand verified values, view simple-polygon.tif in qgis using the tests/base/testdata.qgz project to verify
+        checkArr = np.array([[
+            [20., 10.,  0.,  0.],
+            [10.,  0., 10.,  0.],
+            [ 0., 10., 30., 10.],
+            [ 0.,  0., 10., 10.]
+        ]], dtype=np.float32)
+        np.testing.assert_array_equal(arr, checkArr)
 
 # Used for debugging
 if __name__ == "__main__":
     test_simple_sap_map()
+    test_importance_sap_map()
